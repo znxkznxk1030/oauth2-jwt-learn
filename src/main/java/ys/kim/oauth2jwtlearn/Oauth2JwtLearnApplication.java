8b697ad913594646069f427ce3bb47d3ec30b5b0
ysKim <znxkznxk1030@gmail.com>
2021-12-20 23:14:24 +0900

    spring jwt init

---
+++ src/main/java/ys/kim/oauth2jwtlearn/Oauth2JwtLearnApplication.java
@@ -0,0 +1,13 @@
+package ys.kim.oauth2jwtlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtLearnApplication.class, args);
+	}
+
+}
